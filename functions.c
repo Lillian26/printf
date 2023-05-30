b6b2d864a249df4bce0e1a05d9211ba6fb402635
Lillian Namusisi <lilliannamusisi@gmail.com>
2023-05-30 15:13:24 -0700

    a.out, update functions.c

--- functions.c
+++ functions.c
@@ -20,46 +20,6 @@ int print_char(va_list types, char buffer[], int flags,
 }
 
 /**
- * handle_write_char - prints a string
- * @c: char types.
- * @buffer: Buffer array to handle print
- * @flags: calculates the active flasg
- * @width: get width
- * @precision: precision specifier given
- * @size: size specifier given
- *
- * Return: number of characters printed
- */
-int handle_write_char(char c, char buffer[], int flags,
-		int width, int precision, int size)
-{
-	int i = 0;
-	char padd = ' ';
-
-	UNUSED(precision);
-	UNUSED(size);
-
-	if (flags & F_ZERO)
-		padd = '0';
-	buffer[i++] = c;
-	buffer[i] = '\0';
-
-	if (width > 1)
-	{
-		buffer[BUFF_SIZE - 1] = '\0';
-		for (i = 0; i < width - 1; i++)
-			buffer[BUFF_SIZE - i - 2] = padd;
-		if (flags & F_MINUS)
-			return (write(1, &buffer[0], 1) +
-					write(1, &buffer[BUFF_SIZE - i - 1], width - 1));
-		else
-			return (write(1, &buffer[BUFF_SIZE - i - 1], width - 1) +
-					write(1, &buffer[0], 1));
-	}
-	return (write(1, &buffer[0], 1));
-}
-
-/**
  * print_string - prints a string
  * @types: list of arguments
  * @buffer: buffer array to handle print
